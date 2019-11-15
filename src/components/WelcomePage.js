2ce422c3df7f8eb2f5e9ac83c7ecabba49b97c71
Reece Pierson <piersonreece@gmail.com>
2019-11-15 16:14:31 -0600

    styled it. made it ugly, but it's styled

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,25 @@
 import React from "react";
+import styled from 'styled-components'
+
+const Header = styled.header`
+background: red;
+text-align: center;
+`
+const Img = styled.img`
+margin: 5% auto;
+`
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <Header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
+      </Header>
     </section>
   );
 }
